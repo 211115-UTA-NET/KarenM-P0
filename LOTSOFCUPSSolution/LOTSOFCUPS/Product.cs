b88c7326e1499aa34fd964120fe5c3b17a992914
[martinezk23] <[mkaren2301@gail.com]>
2021-12-13 23:36:49 -0600

    Redo

---
+++ LOTSOFCUPSSolution/LOTSOFCUPS/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTSOFCUPS
+{
+    internal class Product
+    {
+        private int productID { get; set; }
+        string? name;
+        decimal price;
+        int size;
+    }
+
+    public void SmartCups(Product product)
+    {
+        Console.WriteLine("1. Purple 8oz" + "\n" + 
+        public int ProductID { get; set; } = 0;
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int Size { get; set; }
+
+    }
+}
